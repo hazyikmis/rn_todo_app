d73fb8fdae2485a1f863bcf6d590941a89fa0d5f
Halil Azyikmis <hazyikmis@yahoo.com>
2020-08-19 00:40:01 +0200

    TextInput and Button added & styled

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   return (
-    <View>
+    <View style={{ padding: 30 }}>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-evenly",
+          alignItems: "center",
+        }}
+      >
+        <TextInput
+          placeholder="Course Goal"
+          style={{
+            width: "80%",
+            borderColor: "black",
+            borderWidth: 1,
+            padding: 10,
+          }}
+        />
+        <Button title="Add" />
+      </View>
+      <View></View>
       <StatusBar style="auto" />
     </View>
   );
